28cd443d4d7efd41f923f73dbb7f17e0d7e6dcce
Artem <artem3141065122@gmail.com>
2024-11-29 20:34:40 +0400

    Изменения в task3

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -11,43 +11,41 @@
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    while (true) {
 
-        int n, m;
-        std::cout << "Введите n (граница диапазона): ";
-        std::cin >> n;
-        std::cout << "Введите m (количество потоков): ";
-        std::cin >> m;
+    int n, m;
+    std::cout << "Введите n (граница диапазона): ";
+    std::cin >> n;
+    std::cout << "Введите m (количество потоков): ";
+    std::cin >> m;
 
 
-        std::vector<int> numbers = NumbersGenerator::generateNumbers(n);
-        std::vector<int> basicNumbers = EratosthenesSieve::getBasicNumbers(numbers);
-        std::vector<int> primes;
-
+    std::vector<int> numbers = NumbersGenerator::generateNumbers(n);
+    std::vector<int> basicNumbers = EratosthenesSieve::getBasicNumbers(numbers);
+    std::vector<int> primes;
         
-        RangePartition rangePartition(basicNumbers, n, m);
-        primes = rangePartition.findPrimes();
-        std::cout << rangePartition.duration << std::endl;
+
+    RangePartition rangePartition(basicNumbers, n, m);
+    primes = rangePartition.findPrimes();
+    std::cout << rangePartition.duration << std::endl;
 
 
-        BasePartition basePartition(basicNumbers, n, m);
-        primes = basePartition.findPrimes();
-        std::cout << basePartition.duration << std::endl;
+    BasePartition basePartition(basicNumbers, n, m);
+    primes = basePartition.findPrimes();
+    std::cout << basePartition.duration << std::endl;
 
 
-        RangePartitionThreadPool rangePartitionTP(basicNumbers, n, m);
-        primes = rangePartitionTP.findPrimes();
-        std::cout << rangePartitionTP.duration << std::endl;
+    RangePartitionThreadPool rangePartitionTP(basicNumbers, n, m);
+    primes = rangePartitionTP.findPrimes();
+    std::cout << rangePartitionTP.duration << std::endl;
 
 
-        BasePartitionThreadPool basePartitionTP(basicNumbers, n, m);
-        primes = basePartitionTP.findPrimes();
-        std::cout << basePartitionTP.duration << std::endl;
+    BasePartitionThreadPool basePartitionTP(basicNumbers, n, m);
+    primes = basePartitionTP.findPrimes();
+    std::cout << basePartitionTP.duration << std::endl;
 
 
-        FoundPrimes foundprimes(basicNumbers, primes);
-        foundprimes.printToConsole();
-    }
+    FoundPrimes foundprimes(basicNumbers, primes);
+    foundprimes.printToConsole();
 
     return 0;
 }
